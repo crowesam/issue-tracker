ba9235ae484ea06bd9afe504a21232fbae8ef953
Sam Crowe <crowesam@users.noreply.github.com>
2024-11-22 22:50:55 -0500

    build the nav bar

--- issue-tracker/app/NavBar.tsx
+++ issue-tracker/app/NavBar.tsx
@@ -1,13 +1,20 @@
+import { link } from 'fs';
 import Link from 'next/link'
 import React from 'react'
+import { LiaCrowSolid } from "react-icons/lia";
 
 const NavBar = () => {
+    const links =[
+        {href: '/', label: 'Dashboard'},
+        {href: '/issues', label: 'Issues'},
+    ]
     return (
-        <nav className="flex">
-            <Link href="/">Logo</Link>
-                <ul>
-                    <li><Link href="/">Dashboard</Link></li>
-                    <li><Link href="/issues">Issues</Link></li>
+        <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+            <Link href="/"><LiaCrowSolid /></Link>
+                <ul className='flex space-x-6'>
+                    {links.map(link => <Link key={link.href} 
+                    className='text-zinc-500 hover:text-zinc-800 transition-colors'
+                      href={link.href}> {link.label}</Link>)}
                 </ul>
             
         </nav>

fcae61f4d182ab642c336ed80677af448cdc1ebf
Sam Crowe <crowesam@users.noreply.github.com>
2024-11-25 22:52:53 -0500

    Setup prisma

--- issue-tracker/app/NavBar.tsx
+++ issue-tracker/app/NavBar.tsx
@@ -1,9 +1,13 @@
+'use client';
 import { link } from 'fs';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { LiaCrowSolid } from "react-icons/lia";
+import classnames from 'classnames';
 
 const NavBar = () => {
+    const currentPath = usePathname()
     const links =[
         {href: '/', label: 'Dashboard'},
         {href: '/issues', label: 'Issues'},
@@ -12,9 +16,15 @@ const NavBar = () => {
         <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
             <Link href="/"><LiaCrowSolid /></Link>
                 <ul className='flex space-x-6'>
-                    {links.map(link => <Link key={link.href} 
-                    className='text-zinc-500 hover:text-zinc-800 transition-colors'
-                      href={link.href}> {link.label}</Link>)}
+                    {links.map(link => <Link
+                      key={link.href} 
+                      className={classnames({
+                            'text-red-600': currentPath === link.href,
+                            'text-gray-500': currentPath !== link.href,
+                            'hover:text-zinc-800 transition-colors':true
+                      })}
+                      href={link.href}> {link.label}
+                      </Link>)}
                 </ul>
             
         </nav>

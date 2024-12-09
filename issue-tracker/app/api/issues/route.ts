ea9759d69a2946c96a11eff89cfdebd812a055ef
Sam Crowe <crowesam@users.noreply.github.com>
2024-12-09 17:19:08 -0500

    POST-Braden helping!

--- issue-tracker/app/api/issues/route.ts
+++ issue-tracker/app/api/issues/route.ts
@@ -5,7 +5,7 @@ import prisma from '@/prisma/client';
 import { NextResponse } from 'next/server';
 
 const createIssueSchema = z.object({
-    title: z.string() .min(1).max(255),
+    title: z.string().min(1).max(255),
     description: z.string().min(1)
 });
 export async function POST(request: NextRequest) {    

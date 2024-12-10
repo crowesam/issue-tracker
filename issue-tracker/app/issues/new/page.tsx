43c3f687612c28c9453ad76275d9bca42114724d
Sam Crowe <crowesam@users.noreply.github.com>
2024-12-10 09:27:42 -0500

    Added Mark Down Editor iwth Simple MDE

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 
-import { TextField, TextArea, Button} from '@radix-ui/themes';
+import { TextField, Button} from '@radix-ui/themes';
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 import React from 'react'
 
 const NewIssuePage = () => {
@@ -8,7 +10,7 @@ const NewIssuePage = () => {
     <div className='max-w-xl space-y-3'>
         <TextField.Root placeholder='Title'/>                     
  
-		<TextArea size="3" placeholder="Descritpion of issue"/>
+		<SimpleMDE size="3" placeholder="Descritpion of issue"/>
         <Button>Submit New Issue</Button>
     </div>
 

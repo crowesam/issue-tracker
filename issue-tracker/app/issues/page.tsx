d90c6c9349f48206b3085080f17334e4166bbf7b
Sam Crowe <crowesam@users.noreply.github.com>
2024-12-09 22:51:17 -0500

    Middle

--- issue-tracker/app/issues/page.tsx
+++ issue-tracker/app/issues/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
 import { Button } from '@radix-ui/themes'
+import   Link from 'next/link'
+
+
 const IssuesPage = () => {
   return (
-    <div><Button>New Issue</Button></div>
+    <div className= 'maxwidth-w-xl'><Button><Link href='/issues/new'>New Issue</Link></Button></div>
   )
 }
 
